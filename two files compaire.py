2460d7d93762dcfa54f7b09fe778d030c7e73d7b
mariomalak1 <93185168+mariomalak1@users.noreply.github.com>
2022-03-27 12:11:22 +0200

    Add files via upload

--- two files compaire.py
+++ two files compaire.py
@@ -1,17 +1,37 @@
+'''
+Name : Mario Malak Alabd
+College of Computers and Artificial Intelligence
+
+this program, will make you get diffrance between two files and can copy this files to specific location
+
+the program want from you first to enter path of folder that has the files on it directly
+or has some folders that each folder has some of your files
+
+and the second folder is the folder that you will compare with it, and finally it will want you to enter path to save this files 
+
+the first folder it's elements will be added or not, not the second folder 
+
+'''
 import os
 import shutil
-####
 ### functions here
 def List_Elements(dir1, counter = 0):
     lis1 = []
-    folders = os.listdir(dir1)
-    for folder in folders:
-        if (os.path.isdir(dir1+"\\"+folder)):
-            for element in (os.listdir(dir1 +"\\" +folder)):
-                lis1.append(element)
-        else:
-            lis1.append(folder)
-    return lis1
+    try:
+        folders = os.listdir(dir1)
+        for folder in folders:
+            if (os.path.isdir(dir1+"\\"+folder)):
+                for element in (os.listdir(dir1 +"\\" +folder)):
+                    lis1.append(element)
+            else:
+                lis1.append(folder)
+        
+        return lis1
+    except FileNotFoundError:
+        return lis1
+    except:
+        print("it was an error please make sure of your entered data")
+        return lis1
 ### the files in folder 1 if itsn't in folder 2 it will be added to it
 ### so this files is in folder 1 
 def Compare_Lists(list1 , list2, counter = 0):
@@ -65,12 +85,20 @@ def copy_operation(lis_dif , path_destenation , dir1 , counter = 0):
     for i in lis_dif:
         counter += 1
         Get_File_source_Copy(i,dir1 , path_destenation , counter , len(lis_dif))
-def Copy_Dif():
+def copy_diff_program():
     folder1 = input("please enter first path of your folder or your files : ")
     folder2 = input("please enter second path of your folder or your files : ")
-    path1 = input("please enter path to save the diffrence files in it : ")
     lis1 = List_Elements(folder1)
     lis2 = List_Elements(folder2)
     lis3 = Compare_Lists(lis1 , lis2)
-    copy_operation(lis3, path1 , folder1)
-Copy_Dif()
+    if ((lis1 == []) and (lis2 == [])):
+        print("please enter valid data ")
+    else:
+        response = input("if you want to copy this files press 1, if you want to show differance files only press 2")
+        if response == "1":
+            path1 = input("please enter path to save the diffrence files in it : ")
+            copy_operation(lis3, path1 , folder1)
+        elif response == "2":
+            print(lis3 = Compare_Lists(lis1 , lis2))
+if __name__ == "__main__":
+    copy_diff_program()
